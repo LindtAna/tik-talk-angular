37d91220982437bdfca880e22b6863c228438f1c
LindtAna <anastasiialindt@gmail.com>
2025-07-20 21:46:10 +0200

    initial commit

---
+++ src/app/data/services/profile.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Profile } from '../interfaces/profile.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  http = inject(HttpClient)
+
+  baseAPIUrl = 'https://icherniakov.ru/yt-course/'
+
+    getTestAccounts(){
+return this.http.get<Profile[]>(`${this.baseAPIUrl}account/test_accounts`)
+}
+}
